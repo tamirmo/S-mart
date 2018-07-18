f35c9bc44724fbac258c475e49aaa1bad00a1e0b
Rotem-PC\Rotem <yunikr@gmail.com>
2018-07-18 09:12:12 +0300

    1. Deleted ClientMain From Server files. 2. Changed ServerMain into MainServer. 3. Add Notes and System.out.println to MainServer

---
+++ Server/Server/src/MainServer.java
@@ -0,0 +1,9 @@
+
+// Starts S-Mart Main Server
+public class MainServer {
+	public static void main(String[] args) {
+		System.out.println("Starts main server\n");
+		Server server = new Server();
+		server.run();
+	}
+}
